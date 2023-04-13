c20d05ec9ebcf119d5e4452c3fae5f0de540e602
Joe Michel <tun69277@temple.edu>
2023-04-13 12:54:55 -0400

    Final changes

--- app/src/main/java/edu/temple/myapplication/MainActivity.kt
+++ app/src/main/java/edu/temple/myapplication/MainActivity.kt
@@ -1,6 +1,7 @@
 package edu.temple.myapplication
 
 import android.content.ComponentName
+import android.content.Intent
 import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -10,33 +11,37 @@ import android.widget.Button
 class MainActivity : AppCompatActivity() {
 
     lateinit var timerBinder: TimerService.TimerBinder
+    var connected= true
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val serviceConnection = object :ServiceConnection{
-            override fun onServiceConnected(p0: ComponentName?, p1: IBinder?) {
+            override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
                 timerBinder= service as TimerService.TimerBinder
+                connected= true
             }
 
-            override fun onServiceDisconnected(p0: ComponentName?) {
-                TODO("Not yet implemented")
+            override fun onServiceDisconnected(name: ComponentName?) {
+                connected= false
             }
-
-            bindService(intent(this,TimerService::class.java))
-
         }
 
-        findViewById<Button>(R.id.startButton).setOnClickListener {
+        bindService(Intent(this, TimerService::class.java), serviceConnection, BIND_AUTO_CREATE)
 
+        findViewById<Button>(R.id.startButton).setOnClickListener {
+            if(connected)
+                timerBinder.start(50)
         }
 
         findViewById<Button>(R.id.pauseButton).setOnClickListener {
-
+            if(connected)
+                timerBinder.pause()
         }
         
         findViewById<Button>(R.id.stopButton).setOnClickListener {
-
+            if(connected)
+                timerBinder.stop()
         }
     }
 }
