8dd091001dccd3656c938e5ad92f01e7aabe860f
Joe Michel <tun69277@temple.edu>
2023-04-13 12:15:38 -0400

    Create serviceConnection

--- app/src/main/java/edu/temple/myapplication/MainActivity.kt
+++ app/src/main/java/edu/temple/myapplication/MainActivity.kt
@@ -1,14 +1,31 @@
 package edu.temple.myapplication
 
+import android.content.ComponentName
+import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.IBinder
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var timerBinder: TimerService.TimerBinder
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val serviceConnection = object :ServiceConnection{
+            override fun onServiceConnected(p0: ComponentName?, p1: IBinder?) {
+                timerBinder= service as TimerService.TimerBinder
+            }
+
+            override fun onServiceDisconnected(p0: ComponentName?) {
+                TODO("Not yet implemented")
+            }
+
+            bindService(intent(this,TimerService::class.java))
+
+        }
 
         findViewById<Button>(R.id.startButton).setOnClickListener {
 
